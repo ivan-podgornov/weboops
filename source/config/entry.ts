fc606e652c4157dae3da022ebae0191e53b8426e
Ivan Podgornov <ivan.podgornov@live.com>
2021-09-17 12:58:28 +0300

    feat: style.css isn't required

--- source/config/entry.ts
+++ source/config/entry.ts
@@ -3,13 +3,16 @@ import path from 'path';
 import type { Entry } from 'webpack';
 
 export const entry = (sourcePath: string): Entry => {
+    const entry: Entry = {
+        main: path.resolve(sourcePath, './javascript/main.js'),
+    };
+
     const styles = [
         path.resolve(sourcePath, './stylesheets/style.css'),
         path.resolve(sourcePath, './stylesheets/style.scss'),
     ].filter((filename) => fs.existsSync(filename)) as [string, string];
 
-    return {
-        styles,
-        main: path.resolve(sourcePath, './javascript/main.js'),
-    };
+    if (styles.length) entry.styles = styles;
+
+    return entry;
 };
