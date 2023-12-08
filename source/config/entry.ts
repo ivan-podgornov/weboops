87f1631238243ca6e2532f75358a82c950a3b965
baronin <baronin.yaroslav@gmail.com>
2023-12-08 17:27:32 +0200

    feat: remove styles entry

--- source/config/entry.ts
+++ source/config/entry.ts
@@ -1,18 +1,8 @@
-import fs from 'fs';
 import path from 'path';
 import type { Entry } from 'webpack';
 
 export const entry = (sourcePath: string): Entry => {
-    const entry: Entry = {
+    return {
         main: path.resolve(sourcePath, './javascript/main.js'),
     };
-
-    const styles = [
-        path.resolve(sourcePath, './stylesheets/main.css'),
-        path.resolve(sourcePath, './stylesheets/main.scss'),
-    ].filter((filename) => fs.existsSync(filename)) as [string, string];
-
-    if (styles.length) entry.styles = styles;
-
-    return entry;
 };
